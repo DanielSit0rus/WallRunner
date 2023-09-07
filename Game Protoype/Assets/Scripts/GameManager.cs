c521cd106d593c6c561f8f9f4244556f767cebe4
Daniel Hasian Lindung Sitorus <u7181732@anu.edu.au>
2023-09-07 17:54:31 +1000

    Added a title text

--- Game Protoype/Assets/Scripts/GameManager.cs
+++ Game Protoype/Assets/Scripts/GameManager.cs
@@ -1,31 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
 using UnityEngine.SceneManagement;
 
-public class ScoreManager : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
     public TextMeshProUGUI scoreValueText;
     public TextMeshProUGUI gameOverText;
+    public TextMeshProUGUI title;
+
     private PlayerController player;
     public float scoreValue = 0f;
     public float pointPersecond = 1f;
 
     public Button restartButton;
-
+    public Button playButton;
     // Start is called before the first frame update
     void Start()
     {
+        title.gameObject.SetActive(false);
+        scoreValueText.gameObject.SetActive(true); // show score text
         player = GameObject.Find("Player").GetComponent<PlayerController>(); // get player object
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(player.gameOver == false)
+        if (player.gameOver == false)
         {
             scoreValueText.text = "Score: " + ((int)scoreValue).ToString(); // updates the score text
             scoreValue += pointPersecond * Time.fixedDeltaTime; // increase the score by every second the game runs
@@ -34,7 +37,6 @@ public class ScoreManager : MonoBehaviour
         {
             GameOver();
         }
-        
     }
     public void GameOver()
     {
@@ -45,5 +47,9 @@ public class ScoreManager : MonoBehaviour
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name); // reload the game
+    }
+    public void playGame()
+    {
+
     }
 }
