61fccd718eb2a4ee9310e5dcf1d651ba9bb46c88
Daniel Hasian Lindung Sitorus <u7181732@anu.edu.au>
2023-09-07 16:15:34 +1000

    Created a game over text and a functional restart game button when player dies

--- Game Protoype/Assets/Scripts/ScoreManager.cs
+++ Game Protoype/Assets/Scripts/ScoreManager.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class ScoreManager : MonoBehaviour
 {
     public TextMeshProUGUI scoreValueText;
+    public TextMeshProUGUI gameOverText;
     private PlayerController player;
     public float scoreValue = 0f;
     public float pointPersecond = 1f;
+
+    public Button restartButton;
+
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.Find("Player").GetComponent<PlayerController>();
+        player = GameObject.Find("Player").GetComponent<PlayerController>(); // get player object
+        
     }
 
     // Update is called once per frame
@@ -24,6 +30,20 @@ public class ScoreManager : MonoBehaviour
             scoreValueText.text = "Score: " + ((int)scoreValue).ToString(); // updates the score text
             scoreValue += pointPersecond * Time.fixedDeltaTime; // increase the score by every second the game runs
         }
+        else
+        {
+            GameOver();
+        }
         
+    }
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true); // show game over text
+        restartButton.gameObject.SetActive(true); // show the restart button
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); // reload the game
     }
 }
