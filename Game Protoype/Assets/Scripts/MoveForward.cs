145ee4da9422899374db24e6932d35e081f9fbcb
Daniel Hasian Lindung Sitorus <u7181732@anu.edu.au>
2023-09-07 13:44:15 +1000

    Created a scrolling and repeating background and wall objects

--- Game Protoype/Assets/Scripts/MoveForward.cs
+++ Game Protoype/Assets/Scripts/MoveForward.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class MoveForward : MonoBehaviour
 {
-    public float speed = 3.0f;
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = 3.0f; // set the initial speed to 3
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.down * Time.deltaTime * speed);
+        transform.Translate(Vector3.down * speed * Time.deltaTime); // make the background to scroll down
     }
 }
--- Game Protoype/Assets/Scripts/RepeatBackground.cs
+++ Game Protoype/Assets/Scripts/RepeatBackground.cs
@@ -5,21 +5,35 @@ using UnityEngine;
 public class RepeatBackground : MonoBehaviour
 {
 
-    public float speed = 4f;
+    public float speed;
     private Vector3 StartPosition;
     // Start is called before the first frame update
     void Start()
     {
         StartPosition = transform.position;
+        speed = 3.0f; // set the initial speed to 3
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*transform.Translate(Vector3.down * speed * Time.deltaTime);
-        if (transform.position.y < -15.5f)
+        transform.Translate(Vector3.down * speed * Time.deltaTime); // make it scroll down
+        
+
+        if (gameObject.CompareTag("Wall"))
         {
-            transform.position = StartPosition;
-        }*/
+            if(transform.position.y < -10)
+            {
+                transform.position = StartPosition; // if the object is a wall, repeat it when the position of y < 10 
+            }
+        }
+        else
+        {
+            if (transform.position.y < -25.39f)
+            {
+                transform.position = StartPosition; // if it is not a wall (background), repeat it when y < -25.39
+            }
+        }
+        
     }
 }
